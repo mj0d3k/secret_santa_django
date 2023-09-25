18cbf821bd9eae62677bb7111504a053a62121f3
Ania Oleksy <a.oleksy.m@gmail.com>
2023-09-25 14:04:46 +0200

    First thoughts on the models

--- project_cl/secret_santa/models.py
+++ project_cl/secret_santa/models.py
@@ -1,1 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=64)
+    date = models.DateField()
+    description = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Participant(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    wishlist = models.TextField(blank=True, null=True) # optional
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length=64)
+    receiver = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant)
+    draw_date = models.DateField()
+    exchange_date = models.DateField()
+    price_range = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class GiftPair(models.Model):
+    giver = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Giver: {self.giver} -> Receiver: {self.receiver} (In group: {self.group} for event: {self.event})'

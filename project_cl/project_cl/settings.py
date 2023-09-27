ef001b474ace4d4fc17aa6923ce97854168d6124
Ania Oleksy <a.oleksy.m@gmail.com>
2023-09-27 13:59:56 +0200

    settings back to safe

--- project_cl/project_cl/settings.py
+++ project_cl/project_cl/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'harribo4ever@gmail.com'
-EMAIL_HOST_PASSWORD = 'btpo azqj dvqz srun'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

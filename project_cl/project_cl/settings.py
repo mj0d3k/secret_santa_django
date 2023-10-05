0463e74990b23ca1fdee319af63a4cdc3a77e060
Ania Oleksy <a.oleksy.m@gmail.com>
2023-10-05 19:43:18 +0200

    logged user working almst perfecly - only event name seems to appear more than one when should be once - tbc

--- project_cl/project_cl/settings.py
+++ project_cl/project_cl/settings.py
@@ -126,7 +126,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'harribo4ever@gmail.com'
-EMAIL_HOST_PASSWORD = 'zdmk onjw kmxs zfwz' #  problem do rozwiązania z wojtkiem
+EMAIL_HOST_PASSWORD = 'rfcy zlsg mmjw xonl' # not real pswrd! - to chane for zmienne srodowiskowe
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

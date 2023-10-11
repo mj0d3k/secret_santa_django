0e75c58d26fdbf5963f76baa36f8c81b7ea03d10
Ania Oleksy <a.oleksy.m@gmail.com>
2023-10-11 15:26:54 +0200

    first working test

--- project_cl/secret_santa/tests/test_views.py
+++ project_cl/secret_santa/tests/test_views.py
@@ -1,8 +1,33 @@
-import random
-from unittest.mock import patch, Mock
-from secret_santa.views import (secret_santa)
+from django.urls import reverse
+from django.http import HttpResponse
+from django.test import TestCase
+import pytest
+from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-# secret_santa/tests/test_views.py
+
+# main function: secret_santa
 
 
-# tests for all the Views 
+
+# tests for every view x2 -> 40
+
+class MainViewTest(TestCase):
+    def test_main_view_status_code(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_response_type(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_main_view_template_used(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertTemplateUsed(response, 'index.html')
--- project_cl/secret_santa/tests/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-# main function: secret_santa
-
-# tests for every view x2 -> 40
-

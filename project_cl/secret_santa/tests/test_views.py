c47969042989e751e5aedea16c4f4c706fcf2b9f
Ania Oleksy <a.oleksy.m@gmail.com>
2023-10-12 13:59:57 +0200

    long night of test coding

--- project_cl/secret_santa/tests/test_views.py
+++ project_cl/secret_santa/tests/test_views.py
@@ -1,3 +1,4 @@
+import random
 from django.urls import reverse
 from django.http import HttpResponse
 from django.test import TestCase
@@ -5,7 +6,10 @@ import pytest
 from django.test import Client
 from django.contrib.auth.models import User
 from secret_santa.models import Group, Event, Participant
-
+from django.contrib.auth import authenticate, login, logout
+from secret_santa.forms import EventForm, GroupForm, ParticipantForm
+from secret_santa.views import secret_santa
+from django.core import mail
 
 
 ### main views ###
@@ -97,5 +101,135 @@ class GameViewTests(TestCase):
         }
         response = self.client.post(reverse('new-game'), data)
         self.assertEqual(response.status_code, 302)
+
+
+class LookupViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_lookup_view_displayed(self):
+        response = self.client.get(reverse('email-lookup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Email Lookup')
+        self.assertContains(response, 'Enter your email')
+
+    def test_lookup_invalid_email(self):
+        response = self.client.post(reverse('email-lookup'), {'email': 'invalid-email'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Enter a valid email address.')
+
+
+class MyGiftPairsViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_my_gift_pairs_no_pairs(self):
+        response = self.client.get(reverse('my-gift-pairs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Your email does not participate in any games yet.')
+
+    def test_my_gift_pairs_with_pairs(self):
+
+        response = self.client.get(reverse('my-gift-pairs'))
+        self.assertEqual(response.status_code, 200)
+
+
+class MainViewTest(TestCase):
+    def test_main_view_status_code(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_response_type(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_main_view_template_used(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_get_login_page(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login_user_view.html')
+
+    def test_post_login_valid_credentials(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpassword'})
+        self.assertRedirects(response, reverse('base'), fetch_redirect_response=False)
+    
+
+class LogoutViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.logout_url = reverse('logout')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_logout_authenticated_user(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.logout_url)
+
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_logout_redirect_for_authenticated_user(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('login'))
+
+        self.assertRedirects(response, reverse('base'))
+
+
+class ChangePasswordViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.change_password_url = reverse('change-password')
+
+    def test_change_password_redirects_to_login(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        self.client.post(self.change_password_url, {
+            'old_password': 'testpassword',
+            'new_password1': 'newpassword123',
+            'new_password2': 'newpassword123',
+        })
+
+
+class DeleteAccountViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.delete_account_url = reverse('delete_account', args=[self.user.id])
+
+    def test_delete_account_redirects_to_index(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.post(self.delete_account_url)
+
+        self.assertEqual(response.status_code, 302)
+
+        self.assertRedirects(response, reverse('index'))
+
+    def test_user_can_delete_account(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.get(self.delete_account_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'user_delete_confirm.html')
 
 

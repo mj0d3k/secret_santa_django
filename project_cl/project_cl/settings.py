ae010fd7ded11f375495bde04b3052461f837afc
Ania Oleksy <a.oleksy.m@gmail.com>
2023-10-14 23:39:14 +0200

    settings changes installed apps "project_cl.secret_santa"

--- project_cl/project_cl/settings.py
+++ project_cl/project_cl/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'secret_santa',
+    'project_cl.secret_santa',
 ]
 
 MIDDLEWARE = [
